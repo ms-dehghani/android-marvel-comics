6224c86233ef17f8b495754d9783cf50740f7f9d
sadegh1371 <sadegh.dehghani1992@gmail.com>
2024-02-04 22:55:54 +0330

    Show image

--- app/src/main/java/ir/training/marvelcomics/main/view/widgets/items/ComicListItem.kt
+++ app/src/main/java/ir/training/marvelcomics/main/view/widgets/items/ComicListItem.kt
@@ -1,8 +1,11 @@
 package ir.training.marvelcomics.main.view.widgets.items
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,30 +14,49 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
 import ir.training.marvelcomics.domain.model.ComicItem
+import ir.training.marvelcomics.utli.helper.ImageHelper
+import ir.training.marvelcomics.utli.helper.image.ImageSize
 
 @Composable
 fun ComicListItem(
     comicItem: ComicItem,
     onComicClicked: (id: Int) -> Unit,
 ) {
+    Log.d("sssssss" , ImageHelper.getThumbnailUrl(
+        path = comicItem.coverUrlPath,
+        extension = comicItem.coverUrlExtension,
+        imageSize = ImageSize.FANTASTIC
+    ))
+
     Column(
         modifier = Modifier
             .padding(16.dp)
             .clickable { onComicClicked(comicItem.id) },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = rememberImagePainter(data = comicItem.coverUrlPath),
-            contentDescription = "Comic Cover",
+        AsyncImage(
+            model = ImageHelper.getThumbnailUrl(
+                path = comicItem.coverUrlPath,
+                extension = comicItem.coverUrlExtension,
+                imageSize = ImageSize.FANTASTIC
+            ),
+            contentDescription = "Comic Cover ${comicItem.id}",
+            contentScale = ContentScale.FillBounds,
             modifier = Modifier
                 .size(200.dp)
+                .aspectRatio(0.8f)
                 .clip(shape = RoundedCornerShape(15.dp))
+                .background(color = androidx.compose.ui.graphics.Color.LightGray)
+
         )
         Text(
             text = comicItem.title,
@@ -42,18 +64,7 @@ fun ComicListItem(
             fontSize = 20.sp,
             maxLines = 2,
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 8.dp)
-        )
-        Text(
-            text = comicItem.publishedDate,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = 8.dp)
-        )
-        Text(
-            text = comicItem.writer,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis,
             modifier = Modifier.padding(top = 8.dp)
         )
     }
---
+++ app/src/main/java/ir/training/marvelcomics/utli/helper/image/ImageSize.kt
@@ -0,0 +1,10 @@
+package ir.training.marvelcomics.utli.helper.image
+
+enum class ImageSize(val value: String) {
+    SMALL("small"),
+    MEDIUM("medium"),
+    LARGE("large"),
+    X_LARGE("xlarge"),
+    FANTASTIC("fantastic"),
+    AMAZING("amazing")
+}
